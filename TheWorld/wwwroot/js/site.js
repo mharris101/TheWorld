6b002090c29c62800780321aa2b3f6bd3b4ccd76
Matt Harris <matt.harris@dudesolutions.com>
2017-09-14 00:21:13 -0400

    some javascript to change the name and adding bower

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// site.js
+
+(function() {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Matt Harris";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = () => {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = () => {
+        main.style.backgroundColor = "";
+    };
+})();
